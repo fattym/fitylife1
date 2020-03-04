9bcc08d8e17739231f913c7e347a5c42e2cd8e14
fattym <jnganga288@gmail.com>
2020-03-04 12:41:28 +0300

    added new files

---
+++ reviwe.js
@@ -0,0 +1,6 @@
+$("form").submit(function(){
+    var email = $("#exampleFormControlInput1").val();
+    var message = $("#exampleFormControlTextarea1").val();
+
+    alert(name+ '  we have received your message. Thank you for reaching out to us.')
+});
